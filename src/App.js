a7b0345da53a65261ec5e576586198a279928023
SushiDanya <80346614+SushiDanya@users.noreply.github.com>
2021-12-15 21:20:54 +0300

    change lesson2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import { Form } from './components/Form/form';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
 	const [messageList, setMessageList] = useState([]);
+
+	useEffect(() => {
+		if (messageList[messageList.length - 1]?.author === "HUMAN") {
+			setTimeout(() => {
+				handlAddMessage({ text: "Сообщение доставлено", author: "ROBOT" });
+			}, 1500)
+		}
+	})
 
 	const handlAddMessage = (newMessage) => {
 		setMessageList((prevMessageList) => [...prevMessageList, newMessage]);
--- src/components/Form/form.js
+++ src/components/Form/form.js
@@ -4,22 +4,9 @@ import "./form.css";
 export const Form = ({ onAddMessage, newMessageList }) => {
 	const [value, setValue] = useState("");
 
-	// useEffect(() => {
-	//   console.log("Robot: Сообщение доставлено");
-	// }, [newMessageList]);
-
 	const handleChange = (e) => {
 		setValue(e.target.value);
 	};
-
-	useEffect(() => {
-		if (newMessageList[newMessageList.lenght - 1]?.author === "HUMAN") {
-			setTimeout(() => {
-				onAddMessage({ text: "Сообщение доставлено", author: "ROBOT" });
-			}, 1000);
-		}
-		console.log(newMessageList);
-	}, [newMessageList, onAddMessage]);
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
@@ -37,13 +24,16 @@ export const Form = ({ onAddMessage, newMessageList }) => {
 					</div>
 				))}
 			</div>
-			<input
-				type="text"
-				className="input"
-				value={value}
-				onChange={handleChange}
-				placeholder="Сообщение..."
-			></input>
+			<div className="submit">
+				<textarea
+					type="text"
+					className="input"
+					value={value}
+					onChange={handleChange}
+					placeholder="Сообщение..."
+				></textarea>
+				<button type="submit" className="btn">Отправить</button>
+			</div>
 		</form>
 	);
 };
